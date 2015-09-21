d34323daf076b50972b8605b2000161530d42c16
malnick <malnick@gmail..com>
2015-09-21 11:34:21 -0700

    basic agent rest routes and indicies

--- host/host.go
+++ host/host.go
@@ -8,3 +8,8 @@ func Hosts() string {
 	h, _ := ioutil.ReadFile("/etc/hosts")
 	return string(h)
 }
+
+func Hostname() string {
+	h, _ := ioutil.ReadFile("/etc/hostname")
+	return string(h)
+}
--- main.go
+++ main.go
@@ -1,14 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	log "github.com/Sirupsen/logrus"
+	"github.com/gorilla/mux"
 	host "github.com/malnick/cata-agent/host"
+	"net/http"
 	//	"gopkg.in/yaml.v2"
 )
 
 // Define flags
 var verbose = flag.Bool("v", false, "Define verbose logging output.")
+
+func indexContainer(w http.ResponseWriter, r *http.Request) {
+	log.Debug("Starting /container index")
+	fmt.Println("Container Index")
+}
+
+func indexHost(w http.ResponseWriter, r *http.Request) {
+	log.Debug("Starting /host route")
+	fmt.Println("Host index")
+}
+
+func routeHostname(w http.ResponseWriter, r *http.Request) {
+	log.Debug("Starting /host/hostname route")
+	var p struct {
+		Hostname string `json:"hostname"`
+	}
+	p.Hostname = host.Hostname()
+	if err := json.NewEncoder(w).Encode(p); err != nil {
+		log.Debug("failed to encode json for hostname")
+		panic(err)
+	}
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	log.Debug("Starting / index")
+	fmt.Fprintf(w, "Cata Agent Started on /")
+}
 
 func main() {
 	flag.Parse()
@@ -21,5 +52,13 @@ func main() {
 		log.Info("Loglevel: Info")
 	}
 
-	log.Info(host.Hosts())
+	// Define our router object
+	router := mux.NewRouter().StrictSlash(true)
+	// Root index
+	router.HandleFunc("/", Index)
+	log.Fatal(http.ListenAndServe(":8080", router))
+	// Host index
+	router.HandleFunc("/host", indexHost)
+	// /container index
+	http.HandleFunc("/host/hostname", routeHostname)
 }

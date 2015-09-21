9031b23b60cf8db51bb5e7c8870dc577159b1b59
malnick <malnick@gmail..com>
2015-09-21 13:04:26 -0700

    mem

--- utils/host.go
+++ utils/host.go
@@ -1,7 +1,8 @@
-package host
+package utils
 
 import (
 	"io/ioutil"
+	"regexp"
 )
 
 func Hosts() string {
@@ -12,4 +13,34 @@ func Hosts() string {
 func Hostname() string {
 	h, _ := ioutil.ReadFile("/etc/hostname")
 	return string(h)
+}
+
+type Mem struct {
+	Free  string
+	Total string
+}
+
+func Memory() (memory Mem) {
+	freeMatch := regexp.Compile("MemFree:*")
+	totalMatch := regexp.Compile("MemTotal:*")
+	file, err := os.Open("/proc/meminfo")
+	if err != nil {
+		log.Debug("Failed to open /proc/meminfo")
+		return memory{
+			"Failed to open /proc/meminfo",
+			"Failed to open /proc/meminfo",
+		}
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if freeMatch.MatchString(scanner.Text()) {
+			memory.Free = scanner.Text()
+		}
+		if totalMatch.MatchString(scanner.Text()) {
+			memory.Total = scanner.Text()
+		}
+	}
+	return memory
 }

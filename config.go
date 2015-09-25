a8721ed73cd97adc41c3da97b7de0742586ab496
malnick <malnick@gmail..com>
2015-09-24 18:05:04 -0700

    refactored config

--- config.go
+++ config.go
@@ -13,48 +13,56 @@ type Config struct {
 	Consoles    []string `json:"consoles"`
 	ConsolePort string   `json:"console_port"`
 	SplayTime   string   `json:"splay_time"`
+	EnableApi   string   `json:"enable_api"`
 }
 
+// ParseEnv parses the agent environ and gets CATA_* specific data
 func ParseEnv(c Config) Config {
 	// Create a few matches for our env parsing down the road
 	matchConsole, _ := regexp.Compile("CATA_CONSOLES=*")
 	matchPort, _ := regexp.Compile("CATA_CONSOLE_PORT=*")
 	matchSplay, _ := regexp.Compile("CATA_SPLAY_TIME=*")
+	matchApiBool, _ := regexp.Compile("CATA_ENABLE_API=*")
 	// Parse the env for our config
+	log.Debug("Setting ENV configuration..")
 	for _, e := range os.Environ() {
 
-		// Get the consoles from the env
+		// Cata consoles to report to
 		if matchConsole.MatchString(e) {
-			log.Debug("Found Cata Console Match: ", e)
+			log.Debug("Consoles: ", e)
 			consolesAry := strings.Split(strings.Split(e, "=")[1], ",")
 			for _, console := range consolesAry {
 				c.Consoles = append(c.Consoles, string(console))
 			}
+		} else {
+			c.Consoles = append(c.Consoles, "localhost")
 		}
-		// get ports from the env
+
+		// Cata console port
 		if matchPort.MatchString(e) {
-			log.Debug("Port match found: ", e)
+			log.Debug("Port: ", e)
 			c.ConsolePort = strings.Split(e, "=")[1]
+		} else {
+			c.ConsolePort = "9000"
 		}
+
+		// Splay time configuration
 		if matchSplay.MatchString(e) {
-			log.Debug("Splay time found in ENV: ", e)
+			log.Debug("Splay Time: ", e)
 			c.SplayTime = strings.Split(e, "=")[1]
+		} else {
+			c.SplayTime = "2m"
+		}
+
+		// Enable agent REST API
+		if matchApiBool.MatchString(e) {
+			log.Debug("API Enable: ", e)
+			c.EnableApi = strings.Split(e, "=")[1]
+		} else {
+			c.EnableApi = "true"
 		}
 	}
-	// If the consoles were not passed, set a default
-	if len(c.Consoles) < 1 {
-		log.Debug("Consoles not found, setting default to localhost")
-		c.Consoles = append(c.Consoles, "localhost")
-	}
-	// if ports not passed, set a default
-	if len(c.ConsolePort) < 1 {
-		log.Debug("Port match empty, setting default to 9000")
-		c.ConsolePort = "9000"
-	}
-	if len(c.SplayTime) > 0 {
-		log.Debug("Splay not found in env, setting default to 2m")
-		c.SplayTime = "2m"
-	}
+
 	return c
 }
 

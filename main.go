04970aff11e3a0d94d2cf4324647c542e6c177f3
malnick <malnick@gmail..com>
2015-09-25 13:49:57 -0700

    added -p for port assignment of agent

--- main.go
+++ main.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"net/http"
 )
 
 // Define flags
 var verbose = flag.Bool("v", false, "Define verbose logging output.")
+var agentPort = flag.String("p", "8080", "Agent port")
 
 func main() {
 	// Parse flags
 	flag.Parse()
 	// Parse the config here before doing anything else
 	config := ParseConfig()
+	log.Info("Starting Cata Agent...")
+	log.Info(fmt.Sprintf("Configuration: localhost:%s/", *agentPort))
 	// Start the agent
 	go startAgent(config)
 	// Run the router
 	router := NewRouter()
 	// Handle a failure
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", *agentPort), router))
 }

1698b1c652c6aea7327f48f427b8c6816b8509f2
malnick <malnick@gmail..com>
2015-09-24 17:57:11 -0700

    rm alarm config

--- config.go
+++ config.go
@@ -4,21 +4,12 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"os"
 	"regexp"
-	"strconv"
 	"strings"
 )
-
-type Alarm struct {
-	Name     string `json:"name"`
-	Critical int    `json:"critical"`
-	Warning  int    `json:"warning"`
-	Ok       int    `json:"ok"`
-}
 
 type Config struct {
 	Name        string   `json:"name"`
 	LogLevel    string   `json:"log_level"`
-	Alarms      []Alarm  `json:"alarms"`
 	Consoles    []string `json:"consoles"`
 	ConsolePort string   `json:"console_port"`
 	SplayTime   string   `json:"splay_time"`
@@ -26,25 +17,12 @@ type Config struct {
 
 func ParseEnv(c Config) Config {
 	// Create a few matches for our env parsing down the road
-	matchEnv, _ := regexp.Compile("CATA_ALARM_*")
 	matchConsole, _ := regexp.Compile("CATA_CONSOLES=*")
 	matchPort, _ := regexp.Compile("CATA_CONSOLE_PORT=*")
 	matchSplay, _ := regexp.Compile("CATA_SPLAY_TIME=*")
 	// Parse the env for our config
 	for _, e := range os.Environ() {
-		if matchEnv.MatchString(e) {
-			var newAlarm Alarm
-			log.Debug("New alarm found: ", e)
-			newAlarm.Name = strings.Split(strings.Split(e, "=")[0], "CATA_ALARM_")[1]
-			//	Crit value is first in list
-			crit, _ := strconv.Atoi(strings.Split(strings.Split(e, "=")[1], ",")[0])
-			warn, _ := strconv.Atoi(strings.Split(strings.Split(e, "=")[1], ",")[1])
-			ok, _ := strconv.Atoi(strings.Split(strings.Split(e, "=")[1], ",")[2])
-			newAlarm.Critical = crit
-			newAlarm.Warning = warn
-			newAlarm.Ok = ok
-			c.Alarms = append(c.Alarms, newAlarm)
-		}
+
 		// Get the consoles from the env
 		if matchConsole.MatchString(e) {
 			log.Debug("Found Cata Console Match: ", e)
